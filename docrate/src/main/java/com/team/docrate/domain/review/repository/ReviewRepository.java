64ba529c01f8785f1e4e2f85022723d81b816c59
Kimwooyeon0032 <dndus385@naver.com>
2026-04-02 22:19:43 +0900

    feat: 리뷰 수정 및 삭제 기능 구현

--- docrate/src/main/java/com/team/docrate/domain/review/repository/ReviewRepository.java
+++ docrate/src/main/java/com/team/docrate/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,7 @@
 package com.team.docrate.domain.review.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +18,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     
  // 전체리스트(평균 계산)
     List<Review> findAllByDoctorId(Long doctorId);
-    
-    List<Review> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+
+ // 리뷰 ID와 작성자 ID가 일치하는 리뷰를 찾는 메서드 (권한 확인용)
+    Optional<Review> findByIdAndUserId(Long id, Long userId);
 }
